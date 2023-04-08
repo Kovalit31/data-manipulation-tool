2e3e1d5230b67c44c529a48269ee1c033a379e2d
Kovalit31 <nonecone20@gmail.com>
2023-04-08 16:50:08 +0300

    Fork patcher, update subproject

--- patch.py
+++ patch.py
@@ -1,8 +1,8 @@
 import os
 import shutil
-from . import patch
+import patcher
 
-PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+PATH = os.path.dirname(__file__)
 ccmd_patch = os.path.join(PATH, "patch.diff")
 ccmd_orig = os.path.join(PATH, "python-customcmd", "customcmd")
 ccmd_orig_dest = os.path.join(PATH, "customcmd-original")
@@ -12,9 +12,8 @@ def main():
     if os.path.exists(ccmd_out):
         shutil.rmtree(ccmd_out)
     shutil.copytree(ccmd_orig, ccmd_out, dirs_exist_ok=True)
-    patch.setdebug()
-    patcher = patch.fromfile(ccmd_patch)
-    patcher.apply(root=PATH)
+    patch = patcher.fromfile(ccmd_patch, debugmode=True)
+    patch.apply(root=PATH)
 
 if __name__ == "__main__":
     main()
